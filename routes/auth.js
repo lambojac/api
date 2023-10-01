9b36330eaaacbf840d5a0e4782d8ddc8874ceacb
adamokennie <adamokennie@gmail.com>
2023-10-01 13:27:57 +0100

    user creation route and controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 import express from "express"
+import {register} from "../controllers/auth.js"
 const router =express.Router();
-router.get("/",(req,res)=>{
-    res.send("hello this i an auth route")
-})
+router.get("/register",register)
 
 export default router
